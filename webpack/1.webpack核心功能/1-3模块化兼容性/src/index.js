c498987587f908b0ac8cfa922c1503d972f811b6
zhangpan <zhangpan.online@outlook.com>
2024-06-24 09:38:17 +0800

    1

---
+++ webpack/1.webpack核心功能/1-3模块化兼容性/src/index.js
@@ -0,0 +1,9 @@
+// const o = require('./es6a.js')
+// console.log(o)
+// console.log(o.a, o.b, o.default)
+// import cjs from './cjsa.js'
+// console.log(cjs)
+var $1 = require('jquery')
+console.log($1)
+import $2 from 'jquery'
+console.log($2)
