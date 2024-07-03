36b378ad5ba48416b7d42493aaf0cdd1619b0ad6
zhangpan <zhangpan.online@outlook.com>
2024-07-03 17:17:45 +0800

    123

--- docs/js/code.js
+++ docs/js/code.js
@@ -408,3 +408,16 @@ function randomArr() {
   Object.getPrototypeOf(arr) === Array.prototype; // true
   arr.__proto__ === Array.prototype; // true
 }
+
+/**
+ * 20. 发布订阅模式
+ * 创建一个对象
+ * 在该对象上创建一个缓存列表（调度中心）
+ * on 方法用来把函数 fn 都加到缓存列表中（订阅者注册事件到调度中心）
+ * emit 方法取到 arguments 里第一个当做 event，根据 event 值去执行对应缓存列表中的函数（发布者发布事件到调度中心，调度中心处理代码）
+ * off 方法可以根据 event 值取消订阅（取消订阅）
+ * once 方法只监听一次，调用完毕后删除缓存函数（订阅一次）
+ */
+{
+
+}
